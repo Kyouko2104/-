b8f2499185fba5c7e791b9be5f8d71096bb77bec
Kyouko2104 <g4brielalcantara@gmail.com>
2019-09-08 23:17:04 -0300

    Initial commit

---
+++ Main.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from json import *
+
+constants = load(open('utils/constants.json'))
+
+class Application():
+
+    def __init__(self, parent):
+        self.parent = parent
+
+        self.load_hud()
+        self.load_images()
+
+        self.label01 = Label(text='スケジュール', bg=['AntiqueWhite1'])
+        self.label01.config(font=('MS Gothic', 44))
+        self.label01.pack(side=TOP)
+
+        self.label02 = Label(text='______________________________________________________________',
+                             bg=['AntiqueWhite1'])
+        self.label02.config(font=('MS Gothic', 44))
+        self.label02.place(relx=-0.1, rely=0.1)
+
+
+
+    def load_hud(self):
+        self.parent.geometry('450x800+600+100')
+        self.parent['bg'] = ['AntiqueWhite1']
+
+    def load_images(self):
+        self.yuiImage = ImageTk.PhotoImage(Image.open(constants['Yui']))
+        self.yuiImageLabel = Label(image=self.yuiImage)
+        self.yuiImageLabel.place(relx=-0.3, rely=-0.3)
+
+
+root = Tk()
+Application(root)
+root.mainloop()
