022b1d6d625a3b35f0473c1f3bd7f5c792a2a539
Tsugami <yslan3000@gmail.com>
2019-09-09 13:14:30 -0300

    Typos

--- Main.py
+++ Main.py
@@ -30,8 +30,8 @@ class Application():
 
     def load_images(self):
         self.yuiImage = ImageTk.PhotoImage(Image.open(constants['Yui']).resize((160, 160), Image.ANTIALIAS))
-        self.yuiImageLabel = Label(image=self.yuiImage, height=150,width=150,bg=)
-        elf.yuiImageLabel.image = self.yuiImage
+        self.yuiImageLabel = Label(image=self.yuiImage, height=150,width=150)
+        self.yuiImageLabel.image = self.yuiImage
         self.yuiImageLabel.place(relx=0.3, rely=0.5)
 
 
