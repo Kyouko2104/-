1b084e2093b29ddc2788a67ca2656260cb5dde69
Kyouko2104 <g4brielalcantara@gmail.com>
2019-09-13 15:20:08 -0300

    Sukejuruu2

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from PIL import ImageTk, Image
+from PIL import Image, ImageTk
 from json import *
 
 constants = load(open('utils/constants.json'))
@@ -10,19 +10,9 @@ class Application():
     def __init__(self, parent): 
         self.parent = parent
 
-        self.load_images()
         self.load_hud()
-
-
-        self.label01 = Label(text='スケジュール', bg=DEFAULT_COLOR)
-        self.label01.config(font=('MS Gothic', 44))
-        self.label01.pack(side=TOP)
-
-        self.label02 = Label(text='______________________________________________________________',
-                             bg=DEFAULT_COLOR)
-        self.label02.config(font=('MS Gothic', 44))
-        self.label02.place(relx=-0.1, rely=0.2)
-
+        self.load_underlines()
+        self.load_images()
 
 
     def load_hud(self):
@@ -30,10 +20,25 @@ class Application():
         self.parent['bg'] = DEFAULT_COLOR
 
     def load_images(self):
-        self.yuiImage = ImageTk.PhotoImage(Image.open(constants['Yui']).resize((160, 160), Image.ANTIALIAS))
-        self.yuiImageLabel = Label(image=self.yuiImage, height=150,width=150,bg=DEFAULT_COLOR)
-        self.yuiImageLabel.image = self.yuiImage
-        self.yuiImageLabel.place(relx=0.3, rely=0.5)
+        self.yuiImage = ImageTk.PhotoImage(Image.open(constants['Yui']).resize((135, 150), Image.ANTIALIAS))
+        self.yuiImageLabel = Label(root, image=self.yuiImage, width=135, height=150, bg=DEFAULT_COLOR)
+        self.yuiImage.image = self.yuiImage
+        self.yuiImageLabel.place(relx=0.25, rely=0.227, anchor=CENTER)
+
+        self.MHImage = ImageTk.PhotoImage(Image.open(constants['MadHatter']))
+        self.MHImage.Label = Label(root, image=self.MHImage, bg=DEFAULT_COLOR)
+        self.MHImage.image = self.MHImage
+        self.MHImage.Label.place(relx=0.3, rely=0.938)
+
+    def load_underlines(self):
+        self.label01 = Label(text='スケジュール', bg=DEFAULT_COLOR)
+        self.label01.config(font=('MS Gothic', 44))
+        self.label01.pack(side=TOP)
+
+        self.label02 = Label(text='______________________________________________________________',
+                             bg=DEFAULT_COLOR)
+        self.label02.config(font=('MS Gothic', 22))
+        self.label02.place(relx=-0.1, rely=0.28)
 
 
 root = Tk()
